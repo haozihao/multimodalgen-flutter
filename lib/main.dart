01de0d0aa55e0c6674b7c57409eefe03371436a4
blueming-2080ti <1016769622@qq.com>
2024-11-23 10:52:26 +0800

    删除冗余

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:app/app.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
 
@@ -8,18 +7,12 @@ import 'app/pieces_ai.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
   // //滚动性能优化 1.22.0
   // GestureBinding.instance.resamplingEnabled = true;
   //加载全局配置文件
   await GlobalConfiguration().loadFromAsset("app_settings");
   runApp(
-    EasyLocalization(
-        supportedLocales: [Locale('en', 'US'), Locale('jp', 'JP')],
-        path: 'assets/translations',
-        // <-- change the path of the translation files
-        fallbackLocale: Locale('jp', 'JP'),
-        child: const BlocWrapper(child: FlutterUnit())),
+      const BlocWrapper(child: FlutterUnit())
   );
   WindowsAdapter.setSize();
 }
